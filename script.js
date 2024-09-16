0172f339a332dde2a848168efe3337ca71dc552b
AshmeetSingh-prog <ashmeetsingh1445c@gmail.com>
2024-09-16 23:48:30 +0530

    Update script.js
    Improved code structure and added additional features

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-// add history in an array and dispaly it
-// remove eval and let js handle the equation
 
 let input = document.getElementById("input");
 let cross = document.getElementById("cross");
 let buttons = document.querySelectorAll(".buttons");
 let history = document.getElementById("history");
+
+let sin = Math.sin ; 
+let cos = Math.cos ; 
+let tan = Math.tan ;
+
 function evaluateExpression(expression) {
+    // Functions 
+    
     try {
         // Create a new function that returns the result of the expression
         const func = new Function('return ' + expression);
         
-        // Execute the function and return the result
         return func();
     } catch (error) {
         // Return an error message if the expression is invalid
@@ -18,32 +22,53 @@ function evaluateExpression(expression) {
     }
 }
 
+
+
+
 Array.from(buttons).forEach((btn)=>{
     btn.addEventListener("click",(e) => {
         let valueT = e.target.innerHTML;
         let string = String(input.value)
 
         if (valueT == "="){
-            console.log(string);
-            let answer = evaluateExpression(input.value);
+            let answer = evaluateExpression(string);
             input.value = answer;
             history.innerHTML=string
-        } else if (valueT == "C"){
+        } else if (valueT == "AC"){
             string = "-"
             input.value= ''
             history.innerHTML= string
             
         } else if(e.target == cross){
-            lastIndex = input.value.length-1;
-            let correctArr = [];
-            for (let a=0; a<lastIndex;a++){
-                correctArr.push(input.value[a]);
-            }
-            let correctValue = correctArr.join('');
-            input.value = correctValue;
-        }        
+            input.value = input.value.slice(0, -1);
+        }   
         else {
+            
+            if (valueT === "x"){
+                valueT ="*";
+            }
+            else if (valueT === "sin()"){
+                valueT = "sin("
+            }
+            else if (valueT === "cos()"){
+                valueT = "cos("
+            }
+            else if (valueT === "tan()"){
+                valueT = "tan("
+            }
+            
             input.value=input.value + valueT;
+
+            // Checking if the last enterd digit is a operator so that the user Cannot enter the same/different operator again 
+            
+            let operators = ["+" , "-" , "*" , "/" , "%"] ;
+            let lastDigit = input.value.charAt(input.value.length-1);
+            let secondlastDigit = input.value.charAt(input.value.length-2);
+            
+            if(operators.includes(lastDigit) && operators.includes(secondlastDigit)){
+                let newvalue = input.value.slice(0,input.value.length -1) 
+                input.value = newvalue             
+            }            
         }
     })
 })
